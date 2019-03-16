14f1941a930fb0197867ae6ae754fe9ca039b5bf
Matan Bright <36795659+matanbright@users.noreply.github.com>
2019-03-16 18:34:02 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	include_once "PHP - HTML static content/htmlHead.php";
+	include_once "PHP - HTML static content/htmlBody_header.php";
+	include_once "PHP - HTML static content/htmlBody_main_toolbars.php";
+	////////////////////////////////////////////////////// Page content file //////////////////////////////////////////////////////
+	include_once "PHP - HTML pages content/index.php";
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	include_once "PHP - HTML static content/htmlBody_footer.php";
+	include_once "PHP - HTML static content/htmlBody_bottom.php";
+?>
